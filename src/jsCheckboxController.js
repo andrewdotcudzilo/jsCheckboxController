21c39faaa7f68218bc0c9ea23ae9ed3c25e4e1a7
Andrew Cudzilo <andrew.cudzilo@gmail.com>
2015-06-29 16:57:37 -0400

    closer to functional

--- src/jsCheckboxController.js
+++ src/jsCheckboxController.js
@@ -1,36 +1,50 @@
 (function($){
+    $.fn.jsCheckboxController=function(controllerId, className, options) {
+        options=$.extend({}, $.fn.jsCheckboxController.defaultOptions, options);
 
-    $.fn.jsCheckboxController=function(a, b, options) {
-        var settings=$.extend({
-            displayCheckAll = 'Check All',
-            displayUncheckAll = 'Uncheck All'
-        }, options);
+        var $controller=$('#'+controllerId);
+        var $targets=$('.'+className);
 
-        var state=false;
-        //test for existing of a and b
-
-        var $a=$('#'+a);
-        var $b=$('.'+b);
-
-        $a.on('click', function(e) {
-            alert('controll click');
+        $controller.on('click', function(e) {
+            handleController(this, className);
             e.preventDefault();
-        }
-        $b.each(function(el) {
-            el.on('click', function(e){
-                alert('item click');
-            });
         });
+       /* $targets.each(function(index, element) {
+            $(this).on('click', function(e) {
+                alert(index+' clicked');
+            });
+        });*/
         //call check all checked
     };
-
-    function getState(obj) { return obj.state; };
-    function setState(obj, state) { obj.state=state;}
-
-
-
-
-    function debug(obj) {
-        alert('hi');
+    $.fn.jsCheckboxController.options={
+        displayCheckAll:'Check All',
+        displayUncheckAll:'Uncheck All',
     };
+    $.fn.jsCheckboxController.obj={
+        state: false
+    };
+
+    function getState() { return $.fn.jsCheckboxController.obj.state; };
+    function setState(state) { $.fn.jsCheckboxController.obj.state=state; };
+    function setDisplay(obj) {
+        if($(obj).nodeName=='INPUT') {
+            if(getState()) obj.val($.fn.jsCheckboxController.options.displayUncheckAll);
+            else obj.val($.fn.jsCheckboxController.displayCheckAll);
+        }
+        else {
+            if(getState())$(obj).html($.fn.jsCheckboxController.options.displayUncheckAll);
+            else $(obj).html($.fn.jsCheckboxController.options.displayCheckAll);
+        }
+    };
+    function setTargets(el) {
+        if(getState()) $(el).each(function() { $(this).prop('checked', true); });
+        else $(el).each(function() { $(this).prop('checked', false); });
+    };
+    function handleController(obj, className) {
+        setDisplay(obj);
+        setTargets($('.'+className));
+        setState(!getState());
+    };
+
+
 }(jQuery));
