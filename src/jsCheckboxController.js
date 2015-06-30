5936f59715e42295bf3011dc7cfec30aa0054ded
Andrew Cudzilo <andrew.cudzilo@gmail.com>
2015-06-30 16:59:10 -0400

    finished prototype

--- src/jsCheckboxController.js
+++ src/jsCheckboxController.js
@@ -9,13 +9,13 @@
             handleController(this, $targets);
             e.preventDefault();
         });
-       /* $targets.each(function(index, element) {
+       $targets.each(function(index, element) {
             $(this).on('click', function(e) {
-                alert(index+' clicked');
+                checkTargets($controller, $targets);
             });
-        });*/
+        });
         //call check all checked
-        setDisplay($('#'+controllerId));
+        setDisplay($controller);
     };
     $.fn.jsCheckboxController.options={
         displayCheckAll:'Check All',
@@ -37,21 +37,21 @@
             else $(obj).html($.fn.jsCheckboxController.options.displayCheckAll);
         }
     };
-    function setTargets(el) {
-        if(getState()) $(el).each(function() { $(this).prop('checked', true); });
-        else $(el).each(function() { $(this).prop('checked', false); });
+    function setTargets(obj) {
+        if(getState()) $(obj).each(function() { $(this).prop('checked', true); });
+        else $(obj).each(function() { $(this).prop('checked', false); });
     };
     function handleController(controller, targets) {
         setState(!getState());
         setDisplay(controller);
         setTargets(targets);
     };
-    function checkTargets(cObj, chObj) {
+    function checkTargets(controller, targets) {
         var allChecked=true;
-        $(chObj).each(function(){
+        $(targets).each(function(){
             if(!$(this).is(':checked')) allChecked=false;
         });
-        if(allChecked)
-
-
+        setState(allChecked);
+        setDisplay(controller);
+    };
 }(jQuery));
